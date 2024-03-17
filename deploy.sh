5edc71d5573d34c4d0d11fd40e3a4f5283c8be98
Brice MBIANDJI <ambiandji@gmail.com>
2024-03-17 15:41:11 +0100

    fixing streamlit start command

--- deploy.sh
+++ deploy.sh
@@ -16,8 +16,6 @@ python3 -m pip install --upgrade pip
 echo "Installing dependency"
 python3 -m pip install -r requirements.txt
 echo "Running the app"
-python3 app.py >> app.log 2>&1 &
-streamlit run web.py >> streamlit.log 2>&1 &
 
 nohup python3 app.py > flask.log &
 nohup streamlit run web.py > streamlit.log &
